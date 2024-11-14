b8b38ef09afb4f6bedbccc2c4644067f7c78e109
CarlUdo <carl@wbsweden.com>
2024-11-14 09:20:48 +0100

    Add onClick handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 export default function Home() {
+
+  const handleClick = () => {
+    alert('clicked')
+  };
+
   return (
     <>
       <h1>Add a couch</h1>
       <input type="text" />
-      <button>Add couch</button>
+      <button onClick={handleClick}>Add couch</button>
     </>
   )
   
