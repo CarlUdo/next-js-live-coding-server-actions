5247c5a9d50882744996d13ac1e5c599401621e7
CarlUdo <carl@wbsweden.com>
2024-11-14 09:26:59 +0100

    Use a server action to handle onClick

---
+++ src/app/actions.ts
@@ -0,0 +1,5 @@
+"use server"
+
+export const handleClick = async () => {
+  console.log("clicked");
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-'use client'
+"use client"
+
+import { handleClick } from "./actions"
 
 export default function Home() {
-
-  const handleClick = () => {
-    alert('clicked')
-  };
-
   return (
     <>
       <h1>Add a couch</h1>
       <input type="text" />
       <button onClick={handleClick}>Add couch</button>
     </>
-  )
-  
+  )  
 }
